6f61de49d39de1f12db5c028fa0b30836aa6c274
INDRA\jberrialesm <jberrialesm@indra.es>
2025-02-03 13:18:17 +0100

    Prueba comandos

---
+++ prueba.c
@@ -0,0 +1,30 @@
+/*----------------------------HEADER---------------------------------------
+*
+* Copyright: EU Proprietary information.
+* Unauthorised distribution, dissemination or disclosure not allowed.
+*
+* Project: Galileo M&C
+*
+* SDD Component: Complete this field
+* SW-CI: GCB422
+* Implemented Requirements: See DDD
+* Language: C
+* History: See Git history
+*
+*------------------------------------------------------------------------*/
+/**
+* @file_name: prueba.c
+* @ver_file: 1.00
+* @commit_parent: 57fa6bc
+* @date_modfied: 2025-02-03 13:18:17 by INDRAjberrialesm
+* @author: Complete this field
+*
+* @brief: Complete this field
+*
+* Source code of the prueba.c SDD component main functions.
+*------------------------------------------------------------------------*/
+
+
+#inculde <stdlib.h>
+
+print ("Hello world");
