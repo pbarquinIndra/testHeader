57fa6bcc5296625d2fecb221471ab11b321cfffd
INDRA\jberrialesm <jberrialesm@indra.es>
2025-02-03 13:15:25 +0100

    Prueba sin espacios

---
+++ test_fichero.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#----------------------------HEADER---------------------------------------
+#
+# Copyright: EU Proprietary information.
+# Unauthorised distribution, dissemination or disclosure not allowed.
+#
+# Project: Galileo M&C
+#
+# SDD Component: Complete this field
+# SW-CI: GCB422
+# Implemented Requirements: See DDD
+# Language: Bash Script
+# History: See Git history
+#
+#-------------------------------------------------------------------------
+#
+#
+# @file_name: test_fichero.sh
+# @ver_file: 1.00
+# @commit_parent: 5228dc9
+# @date_modfied: 2025-02-03 13:15:25 by INDRAjberrialesm
+# @author: Complete this field
+#
+# @brief: Complete this field
+# @param: None
+# @return: None
+#
+#-------------------------------------------------------------------------
+
+
+cd `dirname $0`ass
